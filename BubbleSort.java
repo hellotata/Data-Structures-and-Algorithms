3bad9bca5675c3970ec291d01b5d228bb88a4be8
Richard Ta <richardtabusiness@gmail.com>
2016-11-07 03:05:57 -0800

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -6,7 +6,7 @@ public class BubbleSort {
 		this.theArray = theArray;
 	}
 	
-  // perform the bubble sort on the array
+  	// perform the bubble sort on the array
 	public int[] sort() {
 		for(int i = 0; i < theArray.length - 1; i++) {
 			for(int j = 0; j < theArray.length - 1 - i; j++) {
@@ -18,14 +18,14 @@ public class BubbleSort {
 		return theArray;
 	}
 	
-  // swap the value of the array at index i and index j
+  	// swap the value of the array at index i and index j
 	public void swapValues(int i, int j) {
 		int temp = theArray[i];
 		theArray[i] = theArray[j];
 		theArray[j] = temp;
 	}
 	
-  // prints the array
+  	// prints the array
 	public void print() {
 		for(int i = 0; i < theArray.length; i++) {
 			if(i == theArray.length - 1) {
