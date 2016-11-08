789cb00b53bc70099a69c84ca2d974d1e21f7d20
Richard Ta <richardtabusiness@gmail.com>
2016-11-08 00:14:08 -0800

    Update InsertionSort.java

--- InsertionSort.java
+++ InsertionSort.java
@@ -6,35 +6,35 @@ public class InsertionSort {
 		this.theArray = theArray;
 	}
 	
-    // Sort the array using Insertion Sort
+   	// Sort the array using Insertion Sort
 	public void sort() {
 		
 		for(int i = 1; i < theArray.length; i++) {
 			
-            // This is the number we are using to compare with other numbers in prior indexes. 
+            		// This is the number we are using to compare with other numbers in prior indexes. 
 			int toInsert = theArray[i];
 			
 			int j = i;
 			
-            // j > 0 ensures that we break out of the while loop if we reach the front of the array
-            // theArray[j-1] ensures that we are comparing the value of the index prior to the toInsert index
+            		// j > 0 ensures that we break out of the while loop if we reach the front of the array
+            		// theArray[j-1] ensures that we are comparing the value of the index prior to the toInsert index
 			while(j > 0 && theArray[j-1] > toInsert) {
 				
 				// If the prior index value is greater, change the value and continue comparing until we find
-                // the desired index for the toInsert value
+                		// the desired index for the toInsert value
 				theArray[j] = theArray[j-1];
 				j--;
 				
 			}
 			
-            // Once we break out of the while loop, it is beause we have found the index to insert the toInsert value
+            		// Once we break out of the while loop, it is beause we have found the index to insert the toInsert value
 			theArray[j] = toInsert;
 			
 		}
 		
 	}
 	
-    // Print the array
+    	// Print the array
 	public void print() {
 		for(int i = 0; i < theArray.length; i++) {
 			if(i == theArray.length - 1) {
