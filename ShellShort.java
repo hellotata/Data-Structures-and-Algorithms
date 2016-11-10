249eff13e824dae448ecdee616e8e8adba941a1b
Richard Ta <richardtabusiness@gmail.com>
2016-11-09 19:11:10 -0800

    Update ShellShort.java

--- ShellShort.java
+++ ShellShort.java
@@ -12,49 +12,49 @@ public class ShellSort {
 		
 		int interval = 1;
 		
-        // Here we define the interval
+        	// Here we define the interval
 		while(interval <= theArray.length / 3) {
 			interval = interval * 3 + 1;
 		}
 		
-        // If interval is less than 0, we break out of the sort
+       		// If interval is less than 0, we break out of the sort
 		while(interval > 0) {
 			   
-            // We set the outer to interval because it is where we will beging our sort, and then
-            // we interate from the outer index all the way to the end of the array
+            		// We set the outer to interval because it is where we will beging our sort, and then
+            		// we interate from the outer index all the way to the end of the array
 			for(outer = interval; outer < this.theArray.length; outer++) {
 				
-                // Set the outer index value to temp so that we can insert into the array when we find appropriate location
+                		// Set the outer index value to temp so that we can insert into the array when we find appropriate location
 				temp = theArray[outer];
 				
 				inner = outer;
 				
-                // If the inner is greater than interval - 1, there is no more number in the front of the array to compare
-                // We then compare the temp with the values before the temp that falls in the interval constraint
+                		// If the inner is greater than interval - 1, there is no more number in the front of the array to compare
+                		// We then compare the temp with the values before the temp that falls in the interval constraint
 				while(inner > interval - 1 && theArray[inner - interval] >= temp) {
 					
-                    // Move the values up the interal array until we find an approriate index to insert temp
+                    			// Move the values up the interal array until we find an approriate index to insert temp
 					theArray[inner] = theArray[inner - interval];
 					
-                    // decrement inner each time so that we can move up the array to compare values
+                    			// decrement inner each time so that we can move up the array to compare values
 					inner -= interval;
 					
 				}
 				
-                // Once we reach here, we have found the location to insert the temp. The value before this inner index
-                // is smaller than the temp
+                		// Once we reach here, we have found the location to insert the temp. The value before this inner index
+               		 	// is smaller than the temp
 				theArray[inner] = temp;
 				
 			}
 			
-            // Decrement the interval and go again
+            		// Decrement the interval and go again
 			interval = (interval - 1) / 3;
 			
 		}
 		
 	}
 	
-    // Prints the array
+    	// Prints the array
 	public void print() {
 		for(int i = 0; i < theArray.length; i++) {
 			if(i == this.theArray.length - 1) {
